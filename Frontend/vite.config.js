b2a8c5f3dd3bb899657202caff5c09342858270c
sejalchhapre02122003 <sejalchhapre123@gmail.com>
2026-07-30 11:19:41 +0530

    modifi

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -21,4 +21,22 @@ export default defineConfig({
     port: 5173,
     host: true,
   },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('lucide-react')) {
+              return 'lucide-icons';
+            }
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 });
